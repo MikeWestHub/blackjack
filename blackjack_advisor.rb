3c44b9edaa301d7086f5d2661a0bb36a1952264b
Mike West <westmd23@gmail.com>
2016-03-09 15:52:53 -0500

    Beginning pseudo code

---
+++ blackjack_advisor.rb
@@ -0,0 +1,68 @@
+# dealer = [2, 3, 4, 5, 6, 7, 8, 9, 10, A]
+# first_hand = [2, 3, 4, 5, 6, 7, 8, 9, 10, A]
+# second_had = [2, 3, 4, 5, 6, 7, 8, 9, 10, A]
+
+
+  puts "What is your first card?"
+    first_card = gets.chomp
+
+  puts "What is your second card?"
+    second_card = gets.chomp
+
+  puts "What is the dealer showing?"
+    dealer_card = gets.chomp
+
+    if first_card || second_card == "A"
+      puts "hard"
+    elsif first_card == second_card
+      puts "pair"
+    else
+      puts "hard"
+    end
+
+  puts = first_card
+  puts = second_card
+  puts = dealer_card
+
+#Hard
+# h_5 =
+# h_6 =
+# h_7 =
+# h_8 =
+# h_9 =
+# h_10 =
+# h_11 =
+# h_12 =
+# h_13 =
+# h_14 =
+# h_15 =
+# h_16 =
+# h_17 =
+# h_18 =
+# h_19 =
+# h_20 =
+# h_21 =
+#
+# #Soft
+# s_13 =
+# s_14 =
+# s_15 =
+# s_16 =
+# s_17 =
+# s_18 =
+# s_19 =
+# s_20 =
+# s_21 =
+#
+# #Pair
+# p_2 =
+# p_3 =
+# p_4 =
+# p_5 =
+# p_6 =
+# p_7 =
+# p_8 =
+# p_9 =
+# p_10 =
+# p_A =
+#
