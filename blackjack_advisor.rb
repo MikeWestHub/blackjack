d5c353efa8976b59019d5eebe3e3c16a7d8ffc9f
Mike West <westmd23@gmail.com>
2016-03-11 14:20:59 -0500

    finished pair table and fixed control flow for if else.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -20,10 +20,12 @@ second_card = ace(second)
 dealer_card = ace(dealer).to_i
 
 hand = first_card.to_i + second_card.to_i
+pair_var = first_card.to_i
 
 h = "Hit!"
 dh = "Don't hit!"
 s = "Stay!"
+sp = "Split!"
 
 #each hash in hard represents dealer_card
 hard = {
@@ -51,9 +53,25 @@ soft = {
   10 => {13 => h, 14 => h, 15 => h, 16 => h, 17 => h, 18 => h, 19 => s, 20 => s, 21 => s},
   11 => {13 => h, 14 => h, 15 => h, 16 => h, 17 => h, 18 => s, 19 => s, 20 => s, 21 => s}
 }
+#each hash in pair represents dealer_card
+pair = {
+  2 => {2 => sp, 3 => sp, 4 => h, 5 => dh, 6 => sp, 7 => sp, 8 => sp, 9 => sp, 10 => s, 11 => sp},
+  3 => {2 => sp, 3 => sp, 4 => h, 5 => dh, 6 => sp, 7 => sp, 8 => sp, 9 => sp, 10 => s, 11 => sp},
+  4 => {2 => sp, 3 => sp, 4 => sp, 5 => h, 6 => sp, 7 => sp, 8 => sp, 9 => sp, 10 => s, 11 => sp},
+  5 => {2 => sp, 3 => sp, 4 => sp, 5 => h, 6 => sp, 7 => sp, 8 => sp, 9 => sp, 10 => s, 11 => sp},
+  6 => {2 => sp, 3 => sp, 4 => sp, 5 => h, 6 => sp, 7 => sp, 8 => sp, 9 => sp, 10 => s, 11 => sp},
+  7 => {2 => sp, 3 => sp, 4 => h, 5 => dh, 6 => sp, 7 => sp, 8 => sp, 9 => s, 10 => s, 11 => sp},
+  8 => {2 => h, 3 => sp, 4 => h, 5 => dh, 6 => h, 7 => sp, 8 => sp, 9 => sp, 10 => s, 11 => sp},
+  9 => {2 => h, 3 => h, 4 => h, 5 => dh, 6 => h, 7 => h, 8 => sp, 9 => sp, 10 => s, 11 => sp},
+  10 => {2 => h, 3 => h, 4 => h, 5 => h, 6 => h, 7 => s, 8 => sp, 9 => s, 10 => s, 11 => sp},
+  11 => {2 => h, 3 => h, 4 => h, 5 => h, 6 => h, 7 => h, 8 => sp, 9 => s, 10 => s, 11 => sp}
+}
 
 if first_card == "a" || second_card == "a"
   play = soft[dealer_card][hand]
+  puts play
+elsif first_card == second_card
+  play = pair[dealer_card][pair_var]
   puts play
 else
   play = hard[dealer_card][hand]
